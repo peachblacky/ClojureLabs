facc71c61fe446a62c716ceca4eda4d934090c77
Rostislav Nikolaev <rostislaev@yandex.ru>
2024-02-19 14:13:28 +0700

    Style clean-up and comments

--- src/task1_1.clj
+++ src/task1_1.clj
@@ -4,52 +4,36 @@
   "Appends letter to the end of the seq if the last letter of string is not the same"
   [^String seq letter]
   {:pre [(some? seq) (some? letter)]}
-  (
-    if (.endsWith seq letter)
-    nil
-    (str seq letter)
-    )
-  )
+  (if (.endsWith seq letter) nil (str seq letter)))         ;returning appended string if the last char differs from the new one
 
 (defn append-letters
   "Returns all possible one-letter extensions for a give seq"
   ([seq alphabet]
    {:pre [some? seq]}
-   (if (empty? alphabet)
-     nil
-     (let [extended-seq (append-letter seq (first alphabet))]
+   (if (empty? alphabet)                                    ;recursion exit condition
+     `()
+
+     (let [extended-seq (append-letter seq (first alphabet)) ;extending seq by first letter of alphabet
+           rest-alphabet-extensions (append-letters seq (rest alphabet))] ;all other symbols from alphabet extensions
        (if (some? extended-seq)
-         (cons (append-letter seq (first alphabet)) (append-letters seq (rest alphabet)))
-         (append-letters seq (rest alphabet)))
-       )
-     )
-   )
-  ([alphabet] alphabet)
-  )
+         (cons extended-seq rest-alphabet-extensions)
+
+         rest-alphabet-extensions)))))
 
 (defn append-seq
   "Returns all string of fixed size from given alphabet without two identical consecutive symbols"
-  (
-   [sequences alphabet n cur-len]
+  ([sequences alphabet n cur-len]
    {:pre [some? sequences]}
-   (
-     if (empty? sequences)
-     nil
-     (if (>= cur-len n)
-       sequences
-       (let [seq-extensions (append-letters (first sequences) alphabet)]
-         (concat (append-seq seq-extensions alphabet n (+ cur-len 1)) (append-seq (rest sequences) alphabet n cur-len))
-         )
-       )
-     )
-   )
+   (if (empty? sequences)                                   ;second recursion branch exit condition
+     `()
 
-  (
-   [alphabet n]
-   (append-seq (list "") alphabet n 0)
-   )
-  )
+     (if (= cur-len n)                                      ;first recursion branch exit condition
+       sequences
+
+       (let [seq-extensions (append-letters (first sequences) alphabet)]
+         (concat (append-seq seq-extensions alphabet n (+ cur-len 1)) (append-seq (rest sequences) alphabet n cur-len))))))
+
+  ([alphabet n] (append-seq (list "") alphabet n 0)))       ;initial function call
 
 (let [alphabet (list "a", "b", "c")]
-  (println (append-seq alphabet 3))
-  )
+  (println (append-seq alphabet 3)))
