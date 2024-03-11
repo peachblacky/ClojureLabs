0f790471fdcdae5efffdef31dbc8f7340c349f88
Rostislav Nikolaev <rostislaev@yandex.ru>
2024-03-11 19:14:26 +0700

    Task 1-2 WIP

---
+++ src/task1_2.clj
@@ -0,0 +1,40 @@
+(ns task1-2)
+
+(defn append-letter
+  "Appends letter to the end of the seq if the last letter of string is not the same"
+  [seq letter]
+  {:pre [(some? seq) (some? letter)]}
+  (if (.endsWith seq letter) nil (str seq letter)))         ;returning appended string if the last char differs from the new one
+
+(defn append-letters
+  "Returns all possible one-letter extensions for a give seq"
+  ([seq alphabet acc]
+   {:pre [some? seq]}
+   (if (empty? alphabet)                                    ;recursion exit condition
+     acc
+
+     (let [extended-seq (append-letter seq (first alphabet)) ;extending seq by first letter of alphabet
+           ]
+       (if (some? extended-seq)
+         (recur seq (rest alphabet) (cons extended-seq acc))
+
+         (recur seq (rest alphabet) acc))))))
+
+(defn append-seq
+  "Returns all string of fixed size from given alphabet without two identical consecutive symbols"
+  ([sequences alphabet n cur-len]
+   {:pre [some? sequences]}
+   (if (empty? sequences)                                   ;second recursion branch exit condition
+     `()
+
+     (if (= cur-len n)                                      ;first recursion branch exit condition
+       sequences
+
+       (let [unique-alphabet (set alphabet)
+             seq-extensions (append-letters (first sequences) unique-alphabet `())]
+         (concat (append-seq seq-extensions unique-alphabet n (+ cur-len 1)) (append-seq (rest sequences) unique-alphabet n cur-len))))))
+
+  ([alphabet n] (append-seq (list "") alphabet n 0)))       ;initial function call
+
+
+(println (append-seq (list "a" "b" "c") 2))
---
+++ test/task1_2_test.clj
@@ -0,0 +1,4 @@
+(ns task1_2_test
+  (:require
+    [clojure.test :as test])
+  (:use [task1-2 :only (append-seq)]))
